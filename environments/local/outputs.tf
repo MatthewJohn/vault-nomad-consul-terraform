76782fdaca4f14a829a1a0ad9c0dbf05c7d53090
Matthew John <matthew@dockstudios.co.uk>
2023-05-18 08:01:48 +0100

    Add output for AWS config

--- environments/local/outputs.tf
+++ environments/local/outputs.tf
@@ -13,3 +13,32 @@ output "terraform_access_key" {
 output "terraform_secret_key" {
   value = nonsensitive(module.s3_configure.terraform_secret_key)
 }
+
+locals {
+  aws_config = <<EOF
+cat > ~/.aws/config <<EOS
+[profile dockstudios-terraform]
+region = main
+output = json
+
+[profile dockstudios-admin-terraform]
+region = main
+output = json
+EOS
+
+cat ~/.aws/credentials <<EOS
+[dockstudios-terraform]
+aws_access_key_id=${module.s3_configure.terraform_access_key}
+aws_secret_access_key=${nonsensitive(module.s3_configure.terraform_secret_key)}
+
+[dockstudios-admin-terraform]
+aws_access_key_id=${module.s3_configure.admin_terraform_access_key}
+aws_secret_access_key=${nonsensitive(module.s3_configure.admin_terraform_secret_key)}
+EOS
+
+EOF
+}
+
+output "aws_config" {
+  value = local.aws_config
+}
