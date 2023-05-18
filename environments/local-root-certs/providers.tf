6ab692f56057d1ec20017ebe111b738abd60b49d
Matthew John <matthew@dockstudios.co.uk>
2023-05-18 14:51:58 +0100

    Attempt to switch aws provider version to setup policies for intermediate certificates bucket for terraform user

--- environments/local-root-certs/providers.tf
+++ environments/local-root-certs/providers.tf
@@ -9,8 +9,16 @@ provider "aws" {
 
   profile = "dockstudios-admin-terraform"
 
-  s3_use_path_style           = true
+  s3_force_path_style           = true
   skip_requesting_account_id  = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
+}
+
+terraform {
+  required_providers {
+    aws = {
+      version = "3.76.1"
+    }
+  }
 }
