82a24cda942275ade4ffafa5da9b02471eb7e81f
Matthew John <matthew@dockstudios.co.uk>
2023-05-20 16:46:05 +0100

    Set full cert chain when pushing intermediate signed cert

--- modules/consul/datacenter/certificate.tf
+++ modules/consul/datacenter/certificate.tf
@@ -29,7 +29,7 @@ resource "vault_pki_secret_backend_root_sign_intermediate" "this" {
 
 resource "vault_pki_secret_backend_intermediate_set_signed" "this" {
   backend     = vault_mount.this.path
-  certificate = vault_pki_secret_backend_root_sign_intermediate.this.certificate
+  certificate = join("\n", vault_pki_secret_backend_root_sign_intermediate.this.ca_chain)
 }
 
 resource "vault_pki_secret_backend_role" "this" {
