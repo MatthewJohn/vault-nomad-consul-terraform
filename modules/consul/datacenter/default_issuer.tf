a5953e5e1c07b8a3d950a739675eeba2e0dd28ba
Matthew John <matthew@dockstudios.co.uk>
2023-05-20 16:51:24 +0100

    Attempt to use terraform http to obtain issuer

--- modules/consul/datacenter/default_issuer.tf
+++ modules/consul/datacenter/default_issuer.tf
@@ -1,6 +1,17 @@
 # Hack around setting default issuers
 
-data "http" "get_current_issuer"
+data "http" "get_current_issuer" {
+  url = "${var.vault_cluster.address}/v1/${vault_mount.this.path}/issuers"
+
+  request_headers = {
+    Accept = "application/json"
+    "X-Vault-Token" = var.vault_cluster.token
+  }
+
+  method = "LIST"
+
+  insecure = true
+}
 
 data "vault_generic_secret" "pki_default_issuer" {
   depends_on = [
@@ -14,8 +25,14 @@ resource "vault_generic_endpoint" "pki_config_issuers" {
   path = "${vault_mount.this.path}/config/issuers"
 
   data_json = jsonencode({
-    default                       = data.vault_generic_secret.pki_default_issuer.data.default,
+    default                       = jsondecode(data.http.get_current_issuer.response_body)["data"]["keys"][0],
     default_follows_latest_issuer = true,
   })
   disable_delete = true
+
+  lifecycle {
+    ignore_changes = [
+        data_json
+    ]
+  }
 }
