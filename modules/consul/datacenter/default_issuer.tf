177976ff9ec654a9dff989fb4471446cc48eb182
Matthew John <matthew@dockstudios.co.uk>
2023-05-20 17:14:44 +0100

    Initial attempt at moving to curl to obtain issuer

--- modules/consul/datacenter/default_issuer.tf
+++ modules/consul/datacenter/default_issuer.tf
@@ -1,31 +1,27 @@
 # Hack around setting default issuers
 
-data "http" "get_current_issuer" {
-  url = "${var.vault_cluster.address}/v1/${vault_mount.this.path}/issuers"
+data "external" "get_current_issuer" {
+  program = [
+    "curl",
+    "--fail",
+    "-s",
+    "-H", "X-Vault-Token: ${var.vault_cluster.token}",
+    "--cacert", var.vault_cluster.ca_cert_file,
+    "-XLIST",
+    "${var.vault_cluster.address}/v1/${vault_mount.this.path}/issuers"
+  ]
 
-  request_headers = {
-    Accept = "application/json"
-    "X-Vault-Token" = var.vault_cluster.token
-  }
-
-  method = "LIST"
-
-  insecure = true
-}
-
-data "vault_generic_secret" "pki_default_issuer" {
   depends_on = [
     vault_pki_secret_backend_intermediate_set_signed.this
   ]
-
-  path = "${var.root_cert.pki_mount_path}/config/issuers"
 }
+
 # Set default_follows_latest_issuer
 resource "vault_generic_endpoint" "pki_config_issuers" {
   path = "${vault_mount.this.path}/config/issuers"
 
   data_json = jsonencode({
-    default                       = jsondecode(data.http.get_current_issuer.response_body)["data"]["keys"][0],
+    default                       = data.external.get_current_issuer.result.keys[0],
     default_follows_latest_issuer = true,
   })
   disable_delete = true
