f9a745fe47a6c4b7a5486470e767a884cc9eef16
Matthew John <matthew@dockstudios.co.uk>
2023-05-29 10:19:08 +0100

    Increase nomad memory to 512MB

--- modules/virtual_machines/domains.tf
+++ modules/virtual_machines/domains.tf
@@ -75,7 +75,7 @@ module "nomad" {
   ip_address                 = each.value.ip_address
   ip_gateway                 = each.value.ip_gateway
   nameservers                = var.nameservers
-  memory                     = 386
+  memory                     = 512
   disk_size                  = 5000
   base_disk_path             = var.base_disk_path
   hypervisor_hostname        = var.hypervisor_hostname
