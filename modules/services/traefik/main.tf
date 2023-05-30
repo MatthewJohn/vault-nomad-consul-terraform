558e6e3b86c436e12552c9837432832f3865537b
Matthew John <matthew@dockstudios.co.uk>
2023-05-30 20:16:40 +0100

    PRepare for traefik consul integration

--- modules/services/traefik/main.tf
+++ modules/services/traefik/main.tf
@@ -32,8 +32,26 @@ job "traefik" {
           "--api.insecure=true", ### For Test only, please do not use that in production
           "--entrypoints.web.address=:$${NOMAD_PORT_http}",
           "--entrypoints.traefik.address=:$${NOMAD_PORT_admin}",
-          "--providers.nomad=true",
-          "--providers.nomad.endpoint.address=${var.nomad_region.address}"
+          #"--providers.nomad=true",
+          #"--providers.nomad.endpoint.address=${var.nomad_region.address}"
+          "--providers.consulcatalog.connectaware=true",
+          # Make the communication secure by default
+          "--providers.consulcatalog.connectbydefault=true",
+          "--providers.consulcatalog.exposedbydefault=true",
+          "--entrypoints.http=true",
+          "--entrypoints.http.address=:8080",
+          # The service name below should match the nomad/consul service above
+          # and is used for intentions in consul
+          "--providers.consulcatalog.servicename=traefik-ingress",
+          "--providers.consulcatalog.prefix=traefik",
+
+          # Automatically configured by Nomad through CONSUL_* environment variables
+          # as long as client consul.share_ssl is enabled
+          "--providers.consulcatalog.endpoint.address=<socket|address>"
+          "--providers.consulcatalog.endpoint.tls.ca=<path>"
+        #   "--providers.consulcatalog.endpoint.tls.cert=<path>"
+        #   "--providers.consulcatalog.endpoint.tls.key=<path>"
+          "--providers.consulcatalog.endpoint.token=<token>"
         ]
       }
     }
