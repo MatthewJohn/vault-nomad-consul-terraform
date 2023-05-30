5079c302bb37ad097df83c1309b7ff80ea7f2003
Matthew John <matthew@dockstudios.co.uk>
2023-05-30 07:33:50 +0100

    Remove duplicate configs from nomad client

--- modules/nomad/client/container/config.tf
+++ modules/nomad/client/container/config.tf
@@ -162,8 +162,6 @@ consul {
   client_auto_join    = true
 
   client_service_name    = "nomad-${var.region.name}-${var.datacenter.name}-client"
-  #client_auto_join       = true
-  #server_auto_join       = true
   #client_http_check_name = ""
 
 {{ with secret "${var.consul_datacenter.consul_engine_mount_path}/creds/${var.nomad_client_vault_consul_role}" }}
