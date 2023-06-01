55f0c619514331867474c92b30d1f14b3fb14c35
Matthew John <matthew@dockstudios.co.uk>
2023-06-01 07:23:57 +0100

    Update s3 module back to registry

--- examples/local/s3.tf
+++ examples/local/s3.tf
@@ -1,7 +1,7 @@
 
 module "s3" {
   source  = "terraform-registry.dockstudios.co.uk/dockstudios/local-s3/docker"
-  version = "0.0.1"
+  version = "0.0.4"
 
   domain_name = local.domain_name
   root_username = "root"
@@ -10,9 +10,8 @@ module "s3" {
 
 
 module "s3_configure" {
-  #source  = "terraform-registry.dockstudios.co.uk/dockstudios/local-s3/docker//modules/configure"
-  #version = "0.0.1"
-  source = "../../../terraform-modules/local-s3/modules/configure"
+  source  = "terraform-registry.dockstudios.co.uk/dockstudios/local-s3/docker//modules/configure"
+  version = "0.0.4"
 
   domain_name = local.domain_name
   root_username = module.s3.root_username
