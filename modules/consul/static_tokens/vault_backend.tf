52027ceefb7b0bfe3a970c611fbdfb6411302182
Matthew John <matthew@dockstudios.co.uk>
2023-06-02 06:54:23 +0100

    Update vault consul engine to use HTTPS

--- modules/consul/static_tokens/vault_backend.tf
+++ modules/consul/static_tokens/vault_backend.tf
@@ -13,7 +13,8 @@ resource "vault_consul_secret_backend" "this" {
   path        = var.datacenter.consul_engine_mount_path
   description = "Manages the Consul backend for ${var.datacenter.name}"
   address     = var.datacenter.address_wo_protocol
-  # ca_cert     = var.datacenter.root_cert_public_key
+  scheme      = "https"
+  ca_cert     = var.datacenter.root_cert_public_key
   local       = true
   token       = data.consul_acl_token_secret_id.vault_secret_engine.secret_id
 }
