8dffb967572dfad7bc56757496dfe2b3172a911d
Matthew John <matthew@dockstudios.co.uk>
2023-06-06 07:47:09 +0100

    Add additional configuration to global proxy configuration

--- modules/consul/static_tokens/proxy.tf
+++ modules/consul/static_tokens/proxy.tf
@@ -1,8 +1,21 @@
+
+# https://developer.hashicorp.com/consul/docs/connect/config-entries/proxy-defaults
 resource "consul_config_entry" "global_proxy" {
   name = "global"
   kind = "proxy-defaults"
 
   config_json = jsonencode({
     Mode = "transparent"
+    MeshGateway = {
+      # @TODO Investigate deploying mesh gateways
+      Mode = "none"
+    }
+    TransparentProxy = {
+      OutboundListenerPort = 443  # @TODO Verify
+      DialedDirectly       = false  # @TODO Verify
+    }
+    Expose = {
+      Checks = true
+    }
   })
 }
