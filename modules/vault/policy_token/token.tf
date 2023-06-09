464a121f0c5a7f5c0c7529bf85d409bc665c82c7
Matthew John <matthew@dockstudios.co.uk>
2023-06-09 18:11:55 +0100

    Convert policy token to static token with long TTL

--- modules/vault/policy_token/token.tf
+++ modules/vault/policy_token/token.tf
@@ -1,7 +1,7 @@
 resource "vault_token" "this" {
   policies = [vault_policy.this.name]
 
-  period = 356 * 24 * 60 * 60
+  ttl = 356 * 24 * 60 * 60
 
   metadata = {
     "purpose" = "${var.policy_name}"
