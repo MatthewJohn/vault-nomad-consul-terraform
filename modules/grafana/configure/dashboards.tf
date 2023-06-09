97d60af1d48758bdd86565faf3781b15f90a7d8a
Matthew John <matthew@dockstudios.co.uk>
2023-06-09 17:04:47 +0100

    Fix weird dashboard characters and path for dashboard template

--- modules/grafana/configure/dashboards.tf
+++ modules/grafana/configure/dashboards.tf
@@ -3,7 +3,7 @@ resource "grafana_folder" "vcn" {
 }
 
 data "template_file" "nomad_cluster_dashboard" {
-  template = "${file("${path.module}/dashboards/nomad_cluster.tpl")}"
+  template = file("${path.module}/dashboards/nomad_cluster.json")
   vars = {
     victoria_metrics_datasource_id = grafana_data_source.victoria_metrics.id
     loki_datasource_id             = grafana_data_source.loki.id
