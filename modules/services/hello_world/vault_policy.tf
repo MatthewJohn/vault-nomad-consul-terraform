3eacddc128e5c826cd975641581b174d2e229b57
Matthew John <matthew@dockstudios.co.uk>
2023-06-09 07:47:26 +0100

    Fix naming of hello world resources

--- modules/services/hello_world/vault_policy.tf
+++ modules/services/hello_world/vault_policy.tf
@@ -1,4 +1,4 @@
-resource "consul_acl_policy" "traefik" {
+resource "consul_acl_policy" "this" {
   name        = "nomad-job-${var.nomad_region.name}-hello-world"
   datacenters = [
     var.consul_datacenter.name
@@ -8,16 +8,16 @@ resource "consul_acl_policy" "traefik" {
 RULE
 }
 
-resource "vault_consul_secret_backend_role" "traefik" {
+resource "vault_consul_secret_backend_role" "this" {
   name    = "nomad-job-${var.nomad_region.name}-hello-world"
   backend = var.consul_datacenter.consul_engine_mount_path
 
   consul_policies = [
-    consul_acl_policy.traefik.name
+    consul_acl_policy.this.name
   ]
 }
 
-resource "vault_policy" "traefik" {
+resource "vault_policy" "this" {
   name = "nomad-job-${var.nomad_region.name}-hello-world"
 
   policy = <<EOF
