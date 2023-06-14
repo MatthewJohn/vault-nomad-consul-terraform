2333bd029e9f42bba68a8cee2c1f452407d78dfc
Matthew John <matthew@dockstudios.co.uk>
2023-06-14 06:46:17 +0100

    Add health check to consul client node_exporter service
    Issue #18

--- modules/consul/client/container/config.tf
+++ modules/consul/client/container/config.tf
@@ -173,6 +173,13 @@ tls {
 service {
   name = "node_exporter"
   port = 9100
+
+  check {
+    name = "node_exporter"
+    http = "http://localhost:9100"
+    interval = "5s"
+    timeout = "5s"
+  }
 }
 
 connect {
