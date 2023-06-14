7df0fe1637bcb2878de61b7856f97fbb30cb1c56
Matthew John <matthew@dockstudios.co.uk>
2023-06-14 06:01:03 +0100

    Add initial module draft for node exporter
    Issue #18

---
+++ modules/node_exporter/variables.tf
@@ -0,0 +1,25 @@
+
+variable "hostname" {
+  description = "Hostname"
+  type        = string
+}
+
+variable "domain_name" {
+  description = "Domain name"
+  type        = string
+}
+
+variable "docker_username" {
+  description = "SSH username to connect to docker host"
+  type        = string
+}
+
+variable "docker_host" {
+  description = "Docker host to connect to"
+  type        = string
+}
+
+variable "docker_ip" {
+  description = "IP Address of docker host"
+  type        = string
+}
