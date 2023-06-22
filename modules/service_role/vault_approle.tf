c5e89d130a28e841f32b60810c50458637ac202b
Matthew John <matthew@dockstudios.co.uk>
2023-06-22 23:19:09 +0100

    Attempt to allow deployment approle to use application policy to allow it to pass the role to nomad
    Issue #6

--- modules/service_role/vault_approle.tf
+++ modules/service_role/vault_approle.tf
@@ -2,7 +2,14 @@
 resource "vault_approle_auth_backend_role" "deployment" {
   backend        = var.nomad_region.approle_mount_path
   role_name      = "nomad-deployment-job-${var.nomad_region.name}-${var.name}"
-  token_policies = [vault_policy.deployment_policy.name]
+  token_policies = [
+    # Policies provided to deployment role
+    # to perform actions in terraform to perform deployment
+    vault_policy.deployment_policy.name,
+    # Add vault policy for application, to allow the token
+    # to pass the role to nomad for the application
+    vault_policy.application_policy.name
+  ]
 }
 
 
