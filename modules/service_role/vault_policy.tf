fbe7f3fa1efa4e9ecb9cbbc532a3393c7413963c
Matthew John <matthew@dockstudios.co.uk>
2023-06-22 22:46:43 +0100

    Replace permissions for accessor with "self" paths, allowing renew, revokation and lookup
    Issue #6

--- modules/service_role/vault_policy.tf
+++ modules/service_role/vault_policy.tf
@@ -30,10 +30,30 @@ path "auth/token/create"
   }
 }
 
-path "auth/token/lookup-accessor"
-{
-  capabilities = [ "read" ]
+# Allow tokens to look up their own properties
+path "auth/token/lookup-self" {
+    capabilities = ["read"]
 }
+
+# Allow tokens to renew themselves
+path "auth/token/renew-self" {
+    capabilities = ["update"]
+}
+
+# Allow tokens to revoke themselves
+path "auth/token/revoke-self" {
+    capabilities = ["update"]
+}
+
+# Allow a token to look up its own capabilities on a path
+path "sys/capabilities-self" {
+    capabilities = ["update"]
+}
+
+# path "auth/token/lookup-accessor"
+# {
+#   capabilities = [ "read", "update" ]
+# }
 EOF
 }
 
