d9beaa5e18e6ab46e586c99ec73036992b8585b2
Matthew John <matthew@dockstudios.co.uk>
2023-06-26 07:07:14 +0100

    Update terrareg to use service_role
    Issue #6

--- examples/local/services.tf
+++ examples/local/services.tf
@@ -20,6 +20,22 @@ module "hello-world_service_role" {
   nomad_static_tokens = module.nomad_static_tokens
 }
 
+module "terrareg_service_role" {
+
+  source = "../../modules/service_role"
+
+  name = "terrareg"
+
+  nomad_bootstrap     = module.nomad_bootstrap
+  nomad_region        = module.nomad_global
+  nomad_datacenter    = module.nomad_dc1
+  consul_root_cert    = module.consul_certificate_authority
+  consul_datacenter   = module.dc1
+  consul_bootstrap    = module.consul_bootstrap
+  vault_cluster       = module.vault_cluster
+  nomad_static_tokens = module.nomad_static_tokens
+}
+
 
 # Test services for deployment (during a standard application deployment)
 module "hello-world" {
@@ -32,13 +48,7 @@ module "hello-world" {
 module "terrareg" {
   source = "../../modules/services/terrareg"
 
-  nomad_bootstrap   = module.nomad_bootstrap
-  nomad_region      = module.nomad_global
-  nomad_datacenter  = module.nomad_dc1
-  consul_root_cert  = module.consul_certificate_authority
-  consul_datacenter = module.dc1
-  consul_bootstrap  = module.consul_bootstrap
-  vault_cluster     = module.vault_cluster
+  service_role      = module.terrareg_service_role
   traefik           = module.traefik
   nfs               = module.nomad_nfs_dc1
 }
