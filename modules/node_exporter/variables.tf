d719ef8a789e35aa3ed3c54f788958326d261807
Matthew John <matthew@dockstudios.co.uk>
2024-04-05 07:35:44 +0100

    Enable node exporter for vault and update for docker host objects

--- modules/node_exporter/variables.tf
+++ modules/node_exporter/variables.tf
@@ -1,19 +1,7 @@
-
-variable "hostname" {
-  description = "Hostname"
-  type        = string
-}
-
-variable "domain_name" {
-  description = "Domain name"
-  type        = string
-}
-
 variable "docker_host" {
   description = "Docker host configuration"
   type = object({
-    ip_address      = string
-    docker_username = string
-    fqdn            = string
+    username = string
+    fqdn     = string
   })
 }
