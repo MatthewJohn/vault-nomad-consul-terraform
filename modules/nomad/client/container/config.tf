3f7e2bf1b0a6db3b593276b6a19d0fc307b9a264
Matthew John <matthew@dockstudios.co.uk>
2024-04-08 18:31:24 +0100

    Rename nomad client consul service to contain datacenter

--- modules/nomad/client/container/config.tf
+++ modules/nomad/client/container/config.tf
@@ -172,7 +172,7 @@ consul {
   client_auto_join    = true
 
   server_service_name     = "nomad-${var.region.name}-server"
-  client_service_name    = "nomad-${var.region.name}-client"
+  client_service_name    = "nomad-${var.region.name}-${var.datacenter.name}-client"
   #client_http_check_name = ""
 
 {{ with secret "${var.consul_datacenter.consul_engine_mount_path}/creds/${var.nomad_client_vault_consul_role}" }}
