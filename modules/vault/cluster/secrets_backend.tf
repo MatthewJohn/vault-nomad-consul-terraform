003c911b867b700211df5b3c7220b120e41e9fc3
Matthew John <matthew@dockstudios.co.uk>
2024-04-24 06:35:11 +0100

    Add freeipa CA root to vault for services to use

--- modules/vault/cluster/secrets_backend.tf
+++ modules/vault/cluster/secrets_backend.tf
@@ -10,6 +10,12 @@ resource "vault_mount" "deployment_secrets" {
   description = "Service deployment Secrets KV store"
 }
 
+resource "vault_mount" "admin_secrets" {
+  path        = "admin_secrets_kv"
+  type        = "kv-v2"
+  description = "Admin Secrets KV store"
+}
+
 # Secret for storing Terraform s3 access key
 resource "vault_kv_secret_v2" "terraform_aws_credentials" {
   mount               = vault_mount.deployment_secrets.path
@@ -28,3 +34,27 @@ resource "vault_kv_secret_v2" "terraform_aws_credentials" {
     ignore_changes = [data_json]
   }
 }
+
+data "http" "freeipa_root_cert" {
+  url = "http://freeipa.dock.local/ipa/config/ca.crt"
+
+  request_headers = {
+    Accept = "text/plain"
+  }
+}
+
+
+# Secret for FreeIPA CA
+resource "vault_kv_secret_v2" "service_freeipa_ca" {
+  mount               = vault_mount.service_secrets.path
+  name                = "common/freeipa/ca"
+  cas                 = 1
+  delete_all_versions = true
+
+  data_json = jsonencode(
+    {
+      pem = data.http.freeipa_root_cert.response_body
+    }
+  )
+}
+
